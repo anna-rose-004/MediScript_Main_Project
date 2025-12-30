f5057ed0909e4ccea4ef1dfb599181c0c19b396d
Milka Joseph <milkajoseph4141@gmail.com>
2025-12-30 11:23:13 +0530

    Updated Supabase client configuration

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://grcxruwyldnsrmuieqdf.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
